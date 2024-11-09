6b095da59428343c0bc19ca515f7d8f39d3946bb
Karos. <131356071+Krs-Scripts@users.noreply.github.com>
2024-11-09 19:07:30 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -2,30 +2,29 @@ local startWashing = false
 local timeWashing = false
 local currentWashAmount = 0
 
-local function setTimeWashing()
+local function getTimeWashing()
     timeWashing = true
     Wait(cfg.waitRewashing)
     timeWashing = false
 end
 
+local function createBlip(coords, sprite, name)
+    if not coords or not sprite or not name then return end
+    
+    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+    SetBlipSprite(blip, sprite)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(name)
+    EndTextCommandSetBlipName(blip)
+    return blip
+end
+
 local function startWashingMoney()
     if startWashing then
-        lib.notify({
-            title = locale('title_notify'),
-            description = locale('description_start_wash'),
-            type = locale('error_notify')
-        })
-        return
-    end
-
+        lib.notify({ title = locale('title_notify'), description = locale('description_start_wash'), type = locale('error_notify')}) return end
     if timeWashing then
-        lib.notify({
-            title = locale('title_notify'),
-            description = locale('description_time_wash'),
-            type = locale('error_notify')
-        })
-        return
-    end
+        lib.notify({title = locale('title_notify'), description = locale('description_time_wash'), type = locale('error_notify') }) return end
 
     local input = lib.inputDialog(locale('title_menu_dialog'), {
         {type = 'number', label = locale('label_dialog'), min = 1, description = locale('description_dialog'), icon = cfg.iconDialog}
@@ -40,64 +39,55 @@ end
 
 RegisterNetEvent('krs_moneywash:startWashing', function(amount)
     local playerPed = cache.ped
-
     startWashing = true
-
-    lib.notify({
-        title = locale('title_notify'),
-        description = locale('description_start_washing'),
-        type = locale('inform_notify')
-    })
-
+    lib.notify({title = locale('title_notify'), description = locale('description_start_washing'), type = locale('inform_notify')})
     TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_AA_SMOKE")
-
     if lib.progressCircle({
         duration = cfg.washDuration,
         position = cfg.positionProgress,
         label = locale('label_progress'),
         useWhileDead = false,
-        canCancel = false
+        canCancel = false,
+        disable = {
+            car = true,
+            move = true,
+            combat = true
+        },
     }) then
-       
-        lib.notify({
-            title = locale('title_notify'),
-            description = locale('description_finish_wash'),
-            type = locale('inform_notify')
-        })
+        lib.notify({title = locale('title_notify'), description = locale('description_finish_wash'), type = locale('inform_notify')})
     end
-
     startWashing = false
     ClearPedTasksImmediately(playerPed)
-    setTimeWashing()
+    getTimeWashing()
 end)
 
-for k, v in pairs(cfg.positionWashing) do
-    local point = lib.points.new({
+for _, v in pairs(cfg.positionWashing) do
+    if v.active then 
+        createBlip(v.coords, v.sprite, v.name)
+    end
+    lib.zones.sphere({
         coords = v.coords,
-        distance = 3
-    })
-
-    function point:onEnter()
-        lib.showTextUI(locale('enter_textui'), {
-            position = cfg.positionTextUi,
-            icon = cfg.iconTextUi,
-            style = {
-                borderRadius = cfg.borderRadius,
-                backgroundColor = cfg.backgroundColor,
-                color = cfg.color
-            }
-        })
-    end
-
-    function point:onExit()
-        lib.hideTextUI()
-    end
-
-    function point:nearby()
-        DrawMarker(0, self.coords.x, self.coords.y, self.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 255, 255, false, true, 2, false, nil, nil, false)
-
-        if self.currentDistance < 1 and IsControlJustReleased(0, 38) then
-            startWashingMoney()
+        size = vec3(1.6, 1.4, 3.2),
+        rotation = 346.25,
+        debug = false,
+        onExit = function()
+            lib.hideTextUI()  
+        end,
+        onEnter = function()
+            lib.showTextUI(locale('enter_textui')) 
+        end,
+        inside = function(self)
+            DrawMarker(0, self.coords.x, self.coords.y, self.coords.z, 
+            0.0, 0.0, 0.0, 
+            0.0, 0.0, 0.0, 
+            0.5, 0.5, 0.5, 
+            255, 255, 255, 255,
+            false, true, 2, false, nil, nil, false)
+            if IsControlJustReleased(0, 38) then 
+                startWashingMoney()
+            end
         end
-    end
+    })
 end
+
+
